257a71fa62dda84935ebeab1d0ac718216e0eecd
juancamilomontes <jm3876602@gmail.com>
2026-03-07 14:55:27 -0500

    feat: project bootstrap structure

--- BiblioTech/Program.cs
+++ BiblioTech/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShowMainMenu();
+    }
+
+    static void ShowMainMenu()
+    {
+        Console.WriteLine("=== BIBLIOTECH - MENÚ PRINCIPAL ===");
+        
+    }
+}
